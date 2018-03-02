8ba9ab9d2b73606b262838a4e0101288e28346ef
Ségolène FATIER <fatier-segolene@outlook.com>
2018-03-02 14:46:11 +0100

    ajout du projet .net

---
+++ IHM/Helpers/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using IHM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IHM.Helpers
+{
+    class Singleton
+    {
+        MainModelView cMain;
+
+        static Singleton _instance;
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+                _instance = new Singleton();
+            return _instance;
+        }
+
+        public void SetMainWindowViewModel(MainModelView mainWindowViewModel)
+        {
+            cMain = mainWindowViewModel;
+        }
+        public MainModelView GetMainWindowViewModel()
+        {
+            return cMain;
+        }
+    }
+}
+
