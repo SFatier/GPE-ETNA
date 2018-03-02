380540779a9a9fc6956e585a13c9bb7dc45f102d
Ségolène FATIER <fatier-segolene@outlook.com>
2018-03-02 14:45:17 +0100

    suppression de la partie java

--- GPE-ETNA/src/Interface/IAccountDriveRepository.java
+++
@@ -1,9 +0,0 @@
-package Interface;
-
-import java.util.ArrayList;
-
-import fr.gpe.object.AccountDrive;
-
-public interface IAccountDriveRepository  {
-	public  ArrayList<AccountDrive> FindAccountDriveByIdCloud(int id);
-}
