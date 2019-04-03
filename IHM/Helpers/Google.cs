d187d781620629ac6aec436684d0627612bd8272
Segolene Fatier <fatier-segolene@outlook.com>
2019-04-03 21:53:34 +0200

    ajout

---
+++ IHM/Helpers/Google.cs
@@ -0,0 +1,227 @@
+﻿
+using Com.CloudRail.SI;
+using Com.CloudRail.SI.Interfaces;
+using Com.CloudRail.SI.ServiceCode.Commands.CodeRedirect;
+using Com.CloudRail.SI.Services;
+using Com.CloudRail.SI.Types;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Responses;
+using Google.Apis.Download;
+using IHM.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using File = Google.Apis.Drive.v3.Data.File;
+
+
+namespace IHM.Helpers
+{
+    public class GoogleCloud : DriveBase
+    {
+        ICloudStorage serviceCloudStorage = null;
+        private Utilisateur u = Singleton.GetInstance().GetUtilisateur();
+
+        public GoogleCloud()  {
+
+            CloudRail.AppKey = Constant.LicenceCloudRail;
+            GoogleDrive googledrive =  new GoogleDrive(new LocalReceiver(8082), Constant.GoogleKey, Constant.GooGleSecret,"http://localhost:8082/auth", "someState");
+            serviceCloudStorage = googledrive;
+        }
+
+        public override string Connect()
+        {
+            serviceCloudStorage.Login();
+            String result = serviceCloudStorage.SaveAsString();
+            serviceCloudStorage.LoadAsString(result);
+            return result;
+        }
+
+        public override string GetCompteClient()
+        {
+            if (serviceCloudStorage == null)
+                serviceCloudStorage.LoadAsString(u.CrededentielCloudRailGoogle);
+
+            return serviceCloudStorage.GetUserLogin();
+        }
+
+        public override List<Fichier> GetItems()
+        {
+            serviceCloudStorage.LoadAsString(u.CrededentielCloudRailGoogle);
+            List<CloudMetaData> result = serviceCloudStorage.GetChildren("/");
+            List<Fichier> FileList = ConvertMedatadaToFile(result);
+            return FileList.OrderBy(f => f.Nom).ToList(); 
+        }
+
+        public override Fichier GetItemsByPath(string path)
+        {
+            serviceCloudStorage.LoadAsString(u.CrededentielCloudRailGoogle);
+            try
+            {
+                CloudMetaData result = serviceCloudStorage.GetMetadata(path);
+                Fichier File = new Fichier();
+                File.Nom = result.GetName();
+                File.Taille = result.GetSize().ToString();
+                File.path = result.GetPath();
+                File.Type = (result.GetFolder() ? "dossier de fichiers" : (Path.GetExtension(result.GetName()).Split('.').Length != 2 ? "" : Path.GetExtension(result.GetName()).Split('.')[1]));
+                //File.IMG = c.GetImageMetaData().ToString();
+                File.DateDeCreation = new DateTime(result.GetModifiedAt());
+                return File;
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public override List<Fichier> GetItemsFolder(string folderPath)
+        {
+            serviceCloudStorage.LoadAsString(u.CrededentielCloudRailGoogle);
+            List<CloudMetaData> result = serviceCloudStorage.GetChildren(folderPath);
+            List<Fichier> FileList = ConvertMedatadaToFile(result);
+            return FileList.OrderBy(f => f.Nom).ToList();
+        }
+        
+        public override bool CreateFolder( string path, string nameFolder)
+        {
+            try
+            {
+                serviceCloudStorage.LoadAsString(u.CrededentielCloudRailGoogle);
+                serviceCloudStorage.CreateFolder(path + nameFolder);
+                return true;
+            }catch(Exception)
+            {
+                return false;
+            }
+        }
+
+        public override bool Delete( string path)
+        {
+            try
+            {
+                serviceCloudStorage.LoadAsString(u.CrededentielCloudRailGoogle);
+                serviceCloudStorage.Delete(path);
+                return true;
+            }catch(Exception)
+            {
+                return false;
+            }
+        }
+
+        public override bool Download(string pathGoogle, string pathLocal)
+        {
+            serviceCloudStorage.LoadAsString(u.CrededentielCloudRailGoogle);
+
+            try
+            {
+                Stream stream = serviceCloudStorage.Download(pathGoogle);
+
+                using (var fileStream = new FileStream(pathLocal, FileMode.Create, FileAccess.Write))
+                {
+                    stream.CopyTo(fileStream);
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public override bool FolderExists(string path)
+        {
+            serviceCloudStorage.LoadAsString(u.CrededentielCloudRailGoogle);
+            return serviceCloudStorage.Exists("/myFolder");
+        }
+
+        public override List<Fichier> Search(string str)
+        {
+            serviceCloudStorage.LoadAsString(u.CrededentielCloudRailGoogle);
+            List<CloudMetaData> result = serviceCloudStorage.Search( str);
+            List<Fichier> FileList = ConvertMedatadaToFile(result);
+            return FileList.OrderBy(f => f.Nom).ToList(); 
+        }
+
+        public override bool Upload(string path)
+        {
+            try
+            {
+                serviceCloudStorage.LoadAsString(u.CrededentielCloudRailGoogle);
+                String pathToFile = "/" + path.Split(Path.DirectorySeparatorChar).Last();
+
+                String strPhoto = (@path);
+                FileStream fs = new FileStream(strPhoto, FileMode.Open, FileAccess.Read);
+                serviceCloudStorage.Upload(pathToFile, fs, fs.Length, true);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+
+        public override string CreateShareLink( string filepath)
+        {
+            serviceCloudStorage.LoadAsString(u.CrededentielCloudRailGoogle);
+            return serviceCloudStorage.CreateShareLink(filepath);
+        }
+
+        public override bool Watch(string nom, string pathGoogle)
+        {
+            try
+            {
+                serviceCloudStorage.LoadAsString(u.CrededentielCloudRailGoogle);
+                string pathLocal = System.IO.Path.GetTempPath() + nom.Replace(" ", "");
+                if (System.IO.File.Exists(pathLocal))
+                    System.IO.File.Delete(pathLocal);
+
+                Download(pathGoogle, pathLocal);
+                System.Diagnostics.Process.Start(pathLocal);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public override List<Fichier> GetFilesShared()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool getSpace()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool SharingFile(Fichier fichier, Utilisateur utilisateur)
+        {
+            return false;
+        }
+
+        public override string CreateShareLink(Fichier fichier)
+        {
+            try
+            {
+                serviceCloudStorage.LoadAsString(u.CrededentielCloudRailGoogle);
+                string link = serviceCloudStorage.CreateShareLink(fichier.path);
+                return link;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public override List<Fichier> GetItemsNoShared()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IHM/ModelView/Gestion_Projets/AddProjectModelView.cs
@@ -0,0 +1,288 @@
+﻿using IHM.Helpers;
+using IHM.Model;
+using IHM.ViewModel;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace IHM.ModelView
+{
+    public class AddProjectModelView : ObservableObject, IPageViewModel
+    {
+        ObservableCollection<string> _selectedUsers = new ObservableCollection<string>();
+        ObservableCollection<string> _selectedFiles = new ObservableCollection<string>();
+        public ICommand  Save{ get; set; }
+                
+        public AddProjectModelView()
+        {
+            TitrePage = "Ajouter un projet";
+            LoadAction();
+            LstUser = Singleton.GetInstance().GetAllUtilisateur().Where(user => user.Email != Singleton.GetInstance().GetUtilisateur().Email).Select(u => u.Login).ToList() ;
+            LoadFiles();
+        }
+
+       #region [Binding]
+        private string titrePage;
+        public string TitrePage
+        {
+            get { return this.titrePage; }
+            set
+            {
+                if (!string.Equals(this.titrePage, value))
+                {
+                    this.titrePage = value;
+                    RaisePropertyChanged(nameof(TitrePage));
+                }
+            }
+        }
+
+        private string nomProjet;
+        public string NomProjet
+        {
+            get { return this.nomProjet; }
+            set
+            {
+                if (!string.Equals(this.nomProjet, value))
+                {
+                    this.nomProjet = value;
+                    RaisePropertyChanged(nameof(NomProjet));
+                }
+            }
+        }
+
+        private string descriptionProjet;
+        public string DescriptionProjet
+        {
+            get { return this.descriptionProjet; }
+            set
+            {
+                if (!string.Equals(this.descriptionProjet, value))
+                {
+                    this.descriptionProjet = value;
+                    RaisePropertyChanged(nameof(DescriptionProjet));
+                }
+            }
+        }
+
+        private List<string> _lstUser;
+        public List<string> LstUser
+        {
+            get { return _lstUser; }
+            set
+            {
+                if (!string.Equals(this._lstUser, value))
+                {
+                    this._lstUser = value;
+                    RaisePropertyChanged(nameof(LstUser));
+                }
+            }
+        }
+
+        private List<string> _lstFiles;
+        public List<string> LstFiles
+        {
+            get { return _lstFiles; }
+            set
+            {
+                if (!string.Equals(this._lstFiles, value))
+                {
+                    this._lstFiles = value;
+                    RaisePropertyChanged(nameof(LstFiles));
+                }
+            }
+        }
+
+        public ObservableCollection<string> SelectedUsers
+        {
+            get
+            {
+                return _selectedUsers;
+            }
+        }
+        public ObservableCollection<string> SelectedFiles
+        {
+            get
+            {
+                return _selectedFiles;
+            }
+        }
+        #endregion
+
+        #region [Action]
+
+        private void ActionAddProject(object parameter)
+        {
+            if (NomProjet != "" && DescriptionProjet != "")
+            {
+                Projet p = new Projet();
+                p.NomProject = NomProjet;
+                p.Description = DescriptionProjet;
+                p.LstFiles = GetFilesProject();
+                p.LstUser = GetUserProject();
+                p.IcoIsArchived = "notvalidate.png";
+                p.IsprojetFin = false;
+                p.IsprojetEncours = true;
+                p.DateDeCreation = DateTime.Now;
+                Singleton.GetInstance().addProject(p);
+                shareFile(p); //partage les fichiers correspondant avec l'utilisateur (dropbox seulement)
+                Functions.CreateFileProjet();
+                //SendMail(p);//Envoie d'email
+                Singleton.GetInstance().GetHomeModelView().CurrentContentViewModel = new AdminModelView();
+            }
+            else
+            {
+                MessageBox.Show("Veuillez renseigner tous les champs obligatoires");
+            }
+        }
+
+        private void SendMail( Projet p)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+
+                //ajouter les destinataires
+                foreach (string adress in p.LstUser.Select(x => x.Email))
+                {
+                    mail.To.Add(adress);
+                }
+
+                mail.Subject = "[GED ETNA] Creation de projet "+  p.NomProject ;
+                mail.Body = "Vous trouverez ci-dessous le récapitulatif du projet "+p.NomProject;
+
+                for (int i = 0; i < p.LstFiles.Count(); i++) {
+                    mail.Body += " - " + p.LstFiles[i].Nom;
+                    mail.Body += @"<a href="+ p.LstFiles[i].PreviewUrl + "> Voir</a>" +  "<\br>";
+                }
+                
+                //définir l'expéditeur
+                mail.From = new MailAddress("fatier_s@etna-alternance.net", "Contact GED ETNA");
+
+                //définir les paramètres smtp pour l'envoi
+                SmtpClient smtpServer = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 465,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential("fatier_s@etna-alternance.net", "couliana971")
+                };
+                //envoi du mail
+                smtpServer.Send(mail);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+   
+        //partage les fichiers dropbox avec les différents utilisateurs
+        private void shareFile(Projet p)
+        {
+            foreach (Fichier f in p.LstFiles) {      
+                bool result = false;
+
+                foreach (Utilisateur u in p.LstUser)
+                {
+                    Fichier file = Singleton.GetInstance().GetListModelView().driveBaseGoogle.GetItemsByPath(f.path);
+                    if (file != null)
+                    {
+                        //result = Singleton.GetInstance().GetListModelView().driveBaseGoogle.SharingFile(f, u);
+                        result = true; //non connecté
+                    }
+                    else
+                    {
+                        file = Singleton.GetInstance().GetListModelView().driveBaseDropbox.GetItemsByPath(f.path);
+                        result = Singleton.GetInstance().GetListModelView().driveBaseDropbox.SharingFile(f, u);
+                    }
+
+                    if (!result)
+                        MessageBox.Show("Impossible de partager le fichier avec l'utilistauer  " + u.Email);
+                }
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Récupérer les fichiers des drive 
+        /// </summary>
+        private void LoadFiles()
+        {
+            List<string> lst_file_gg = Singleton.GetInstance().GetListModelView().DgFiles_GG.Select(f => f.Nom).ToList();
+            List<string> lst_file_dp = Singleton.GetInstance().GetListModelView().driveBaseDropbox.GetItemsNoShared().Select(f => f.Nom).ToList();
+            List<string> rslt = new List<string>();
+            rslt.AddRange(lst_file_dp);
+            rslt.AddRange(lst_file_gg);
+            LstFiles = rslt;
+        }
+
+        public void LoadAction()
+        {
+            Save = new RelayCommand(ActionAddProject);
+        }
+
+        private List<Utilisateur> GetUserProject()
+        {
+            List<Utilisateur> lst = new List<Utilisateur>();
+            if (SelectedUsers != null)
+            {
+                List<Utilisateur> lstUtilisateur = Singleton.GetInstance().GetAllUtilisateur();
+
+                foreach (var item in SelectedUsers)
+                {
+                    Utilisateur u = lstUtilisateur.FirstOrDefault(user => user.Login == item);
+                    if (u != null)
+                    {
+                        lst.Add(u);
+                    }
+                }
+            }
+            return lst;
+        }
+
+        private List<Fichier> GetFilesProject()
+        {
+            List<Fichier> lst = new List<Fichier>();
+            if (SelectedFiles != null)
+            {
+                List<Fichier> lstdp = Singleton.GetInstance().GetListModelView().driveBaseDropbox.GetItems();
+
+                foreach (var item in SelectedFiles)
+                {
+                    Fichier u = lstdp.FirstOrDefault(f => f.Nom == item);
+                    if (u != null)
+                    {
+                        u.PreviewUrl = Singleton.GetInstance().GetListModelView().driveBaseDropbox.CreateShareLink(u);
+                        lst.Add(u);
+                    }
+                }
+
+                List<Fichier> lstgg = Singleton.GetInstance().GetListModelView().driveBaseGoogle.GetItems();
+
+                foreach (var item in SelectedFiles)
+                {
+                    Fichier u = lstgg.FirstOrDefault(f => f.Nom == item);
+                    if (u != null)
+                    {
+                        u.PreviewUrl = Singleton.GetInstance().GetListModelView().driveBaseGoogle.CreateShareLink(u);
+                        lst.Add(u);
+                    }
+                }
+            }
+            return lst;
+        }
+    }
+}
